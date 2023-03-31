a9c30b7207bd73f6223feca29491dc21a134ad93
RunThem <iccy.fun@outlook.com>
2023-03-31 17:32:21 +0800

    test: add `queue` base test
    Signed-off-by: RunThem <iccy.fun@outlook.com>

---
+++ queue/queue_test.go
@@ -0,0 +1,42 @@
+package queue
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestBase(t *testing.T) {
+	arr := []int{10, 11, 12, 13, 14}
+	que := New[int](10)
+
+	assert.True(t, que.Empty())
+
+	for _, k := range arr {
+		que.Push(k)
+	}
+
+	assert.Equal(t, len(arr), que.Size())
+	assert.Equal(t, arr[0], que.Peek())
+
+	for _, k := range arr {
+		assert.Equal(t, k, que.Pop())
+	}
+}
+
+func TestNewFrom(t *testing.T) {
+	arr := []int{10, 11, 12, 13, 14}
+	que := NewFrom(10, 11, 12, 13, 14)
+
+	assert.Equal(t, len(arr), que.Size())
+	for _, k := range arr {
+		assert.Equal(t, k, que.Pop())
+	}
+
+	que = NewFrom(arr...)
+
+	assert.Equal(t, len(arr), que.Size())
+	for _, k := range arr {
+		assert.Equal(t, k, que.Pop())
+	}
+}
