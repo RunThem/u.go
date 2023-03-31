5240339897c7ec1a6e6c34f5ff8da138ff5ecffc
RunThem <iccy.fun@outlook.com>
2023-03-31 11:01:36 +0800

    test: add `vector` basic tests
    Signed-off-by: RunThem <iccy.fun@outlook.com>

---
+++ vector/vector_test.go
@@ -0,0 +1,32 @@
+package vector
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestBase(t *testing.T) {
+	vec := New[int](10)
+
+	assert.True(t, vec.Empty())
+	assert.Equal(t, 0, vec.Size())
+	assert.Equal(t, 10, vec.Cap())
+
+	vec.Push(12)
+	vec.Push(15)
+	vec.Push(22)
+	assert.False(t, vec.Empty())
+	assert.Equal(t, 3, vec.Size())
+
+	assert.Equal(t, 12, vec.Front())
+	assert.Equal(t, 22, vec.Back())
+
+	assert.Equal(t, 12, vec.At(0))
+	assert.Equal(t, 15, vec.At(1))
+	assert.Equal(t, 22, vec.At(2))
+
+	assert.Equal(t, 22, vec.Pop())
+	assert.Equal(t, 15, vec.Pop())
+	assert.Equal(t, 1, vec.Size())
+}
