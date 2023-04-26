2032308c67156f6eb4e30049aea916b6f7e18f25
RunThem <iccy.fun@outlook.com>
2023-04-26 21:20:23 +0800

    feat: add slice map()
    Signed-off-by: RunThem <iccy.fun@outlook.com>

--- map.go
+++ map.go
@@ -25,3 +25,17 @@ func Values[K comparable, V any](m map[K]V) []V {
 
 	return values
 }
+
+// Map maps an element to another value
+func Map[T any](slice []T, fn func(T) T) {
+	for i, v := range slice {
+		slice[i] = fn(v)
+	}
+}
+
+// MapBy maps an element to another value
+func MapBy[T any](slice []T, fn func(*T)) {
+	for i := range slice {
+		fn(&slice[i])
+	}
+}
