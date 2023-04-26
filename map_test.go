040aaa02bf1dcd73643554e82e31533fed751bc5
RunThem <iccy.fun@outlook.com>
2023-04-26 21:28:35 +0800

    test: add values() test
    Signed-off-by: RunThem <iccy.fun@outlook.com>

--- map_test.go
+++ map_test.go
@@ -9,9 +9,17 @@ import (
 func TestKeys(t *testing.T) {
 	m := map[int]string{1: "1", 2: "2", 3: "3"}
 
-	a := Keys(m)
+	keys := Keys(m)
 
-	assert.True(t, reflect.DeepEqual([]int{1, 2, 3}, a))
+	assert.True(t, reflect.DeepEqual([]int{1, 2, 3}, keys))
+}
+
+func TestValues(t *testing.T) {
+	m := map[int]string{1: "1", 2: "2", 3: "3"}
+
+	values := Values[int, string](m)
+
+	assert.True(t, reflect.DeepEqual([]string{"1", "2", "3"}, values))
 }
 
 func TestMap(t *testing.T) {
